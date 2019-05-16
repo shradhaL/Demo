ea0cabad468a6b092d3207535a88d3015bcdd2b5
shradha lade <shradha.lade@P2C-1SXP6C2.dir.svc.accenture.com>
2019-05-16 16:41:13 +0530

    fdddddgdf

---
+++ Project1/src/main/java/com/practice/Project1/App.java
@@ -0,0 +1,13 @@
+package com.practice.Project1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
